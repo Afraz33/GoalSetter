f6e85509e9d4a07411b478e2ec4af5f89e1494b5
Afraz33 <afraz3301@gmail.com>
2023-02-09 13:15:05 +0500

    Routes added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("chal gaya")
+const express = require('express')
+const dotenv = require('dotenv').config
+const app = express()
+const port = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
+app.use(express.json())
+
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+app.listen(port, ()=>{ console.log(`App listening on port ${port}`)
+
+})
